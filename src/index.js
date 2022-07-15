82fb470daa47cc9bdb92f79a3a9fbc8501993ea6
Theriomich <theriomich@gmail.com>
2022-07-15 19:46:17 -0400

    Arreglamos el problema que no nos aparecian los alert y pusimos DOM en index JS y validacion en Validator, enlazamos ambos JS

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
+import validator from "./validator.js";
 
-// var name = document.getElementById("tarjetName");
-// var tarjet = document.getElementById("tarjetNumber");
-// var date = document.getElementById("date");
-// var cvv = document.getElementById("cvv");
-// var error = document.getElementById ("error");
+var button = document.getElementById("botonPagar");
 
-var form = document.getElementById ("form");
-form.addEventListener("submit",function(evt){
-  evt.preventDefault() });  
-  console.log ("Enviando formulario...")
- 
+//inicio una variable vacia en la que guardaré mi numero de tarjeta:
+let cardNumber = 0;
+//guardo mi input en una variable:
+const cardNumberInput = document.getElementById("tarjetNumber");
 
- //document.getElementById (tarjetNumber).value.addEventListener(input,event)
-//   var mensajesError= [];
+//escucho el evento input para guardar el valor dentro de cardNumber
+if (cardNumberInput !== null) {
+  cardNumberInput.addEventListener("input", (evento) => {
+    cardNumber = evento.target.value;
+  });
+}
 
-//if(name.value === null || name.value === ""){
-   //mensajesError.push("Ingresa tu nombre");
-//}
-
-// if(tarjet.value === null || tarjet.value === ""){
-//   mensajesError.push("Ingresa los numeros de la tarjeta");
-// }
-
-// if(date.value === null || date.value === ""){
-//   mensajesError.push("Ingresa tu fecha");
-// }
-
-// if(cvv.value === null || cvv.value === ""){
-//   mensajesError.push("Ingresa tu cvv");
-// }
-// error.innerHTML=mensajesError.join(", ");
-
-// });
-
-
+if (button !== null) {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("Enviando formulario...");
+    console.log(cardNumber);
+    validator.isValid(cardNumber);
+  });
+}
 
 //1.Recuperar los numeros desde html
 //2.Asegurar que ingresaron solo numeros y que sean 16
@@ -48,5 +36,4 @@ form.addEventListener("submit",function(evt){
 //11.Desarrollar funcion validacion
 //12.Desarrollar Masiky
 
-
-import validator from './validator.js' 
+ 
